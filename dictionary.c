061b6460949aacbd92be11d31f2220fd87f50464
Barry Brown <profbbrown@gmail.com>
2020-11-05 08:05:18 +0000

    Added comments

--- dictionary.c
+++ dictionary.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Load the dictionary of words from the given filename.
+// Return a pointer to the array of strings.
+// Sets the value of size to be the number of valid
+// entries in the array (not the total array length).
 char ** loadDictionary(char *filename, int *size)
 {
 	FILE *in = fopen(filename, "r");
@@ -23,6 +27,8 @@ char ** loadDictionary(char *filename, int *size)
 	return NULL;
 }
 
+// Search the dictionary for the target string
+// Return the found string or NULL if not found.
 char * searchDictionary(char *target, char **dictionary, int size)
 {
     if (dictionary == NULL) return NULL;
